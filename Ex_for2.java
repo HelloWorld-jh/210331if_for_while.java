4fad1bec2afffa0dfeb118277d3b5a8dad83f1d3
HelloWorld-jh <79819812+HelloWorld-jh@users.noreply.github.com>
2021-03-31 18:41:18 +0900

    Add files via upload

---
+++ Ex_for2.java
@@ -0,0 +1,31 @@
+package Week2.day3;
+
+public class Ex_for2 {
+	for (int i=0; i<4; i++) { //0~5 青 积己
+		for (int j = 0 ; j < i; j++) { // 0~5 凯 积己
+			System.out.print(" ");	//傍归 免仿
+		}
+		for (int k=4; k>i; k--) {
+			System.out.print("*");
+		}
+		for (int k=3; k>i; k--) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+
+
+for (int i =0; i<4; i++) {
+	for (int j =3; j>i; j--) {
+		System.out.print("");
+		}
+	for (int k=0; k<= i; k++) {
+		System.out.print("*");
+		
+	}
+	System.out.println();
+}
+}
+}
+
+
